dbf803ef9c3d1da57d615126a093e7e8e2b90964
Ingvild Reymert <py32@nifu.no>
2019-04-08 21:17:24 +0200

    converting from pdf to png to ocr. Rbind to dataframe.

--- Ingvild/tesseract_script.R
+++ Ingvild/tesseract_script.R
@@ -2,17 +2,15 @@ library(tesseract)
 library(tidyverse)
 library(pdftools)
 
-setwd("/Users/thomastallaksen/Documents/R/Prosjekter/Poli Sci 452 - Text as Data/Ingvild/Test")
+setwd("C:/Users/py32/Desktop/OCR/Test/pdf")
 
-#file_list <- dir(path = ".", pattern = "\\.pdf$", full.names = TRUE, recursive = TRUE)
+filelist_pdf <- list.files(pattern = ".*.pdf")
+datalist_pdf <- lapply(filelist_pdf, function(x)pdf_convert(x, dpi = 600))
+           
+filelist_png <- list.files(pattern = ".*.png")
+datalist_png <- lapply(filelist_png, function(x)ocr(x))
 
-pdf_convert("/Users/thomastallaksen/Documents/R/Prosjekter/Poli Sci 452 - Text as Data/Lectures/Lecture 3/Ban_et_al_Newspapers.pdf")
-
-filelist <- list.files(pattern = ".*.png")
-
-datalist <- lapply(filelist, function(x)ocr(x))
-
-datafr = do.call("rbind", datalist) 
+df <- do.call("rbind", datalist_png) 
 
 
 
